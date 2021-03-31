2bf39c43405bcd37edaf734bb7dc870f93653f3b
Damilare <opeyemisamuel222@gmail.com>
2021-03-31 16:07:50 +0100

    rendering comment with status

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,8 +4,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post('/events', async (req, res) => {
   const event = req.body;
+
+  events.push(events);
+
   try {
     await axios.post('http://localhost:4000/events', event);
     await axios.post('http://localhost:4001/events', event);
@@ -16,6 +21,10 @@ app.post('/events', async (req, res) => {
   }
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => console.log('listening on port 4005'));
