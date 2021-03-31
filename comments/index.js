0811fbaf3436597de9dffc4b4665481d2f874c76
Damilare <opeyemisamuel222@gmail.com>
2021-03-31 15:16:42 +0100

    updating comment  service

--- comments/index.js
+++ comments/index.js
@@ -40,8 +40,29 @@ app.post('/posts/:id/comments', async (req, res) => {
   }
 });
 
-app.post('/events', (req, res) => {
-  console.log('received event', req.body.type);
+app.post('/events', async(req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+
+    await axios.post('localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+
+      }
+    })
+  }
 
   res.send({});
 });
